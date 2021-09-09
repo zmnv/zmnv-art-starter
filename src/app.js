75172318a4b89fbe24f6c37259faf37cc720a499
Valeriy <sorption.music@gmail.com>
2021-09-09 10:35:59 +0300

    live server

--- src/app.js
+++ src/app.js
@@ -3,4 +3,4 @@ import './b';
 
 console.log('d', 99212322424424);
 
-document.body.style ='background-color:blue';
+document.body.style ='background-color:yellow';
--- src/b.js
+++ src/b.js
@@ -1,5 +1,2 @@
 import { getRandomHash } from "./random";
 
-
-const lol = getRandomHash();
-console.log('B file', lol);
